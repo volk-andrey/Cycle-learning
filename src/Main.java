c0e665fad63b36ba87d48deac69c7ede03980866
Andrey <andrey.volk.1999@gmail.com>
2024-05-25 01:30:32 +1000

    add fix problem

--- src/Main.java
+++ src/Main.java
@@ -6,31 +6,27 @@ public class Main {
         for (int i = 1; i <= 10; i++) {
             System.out.print(i + " ");
         }
-        System.out.println();
-        System.out.println("----------------------------");
+        System.out.println("\n----------------------------");
 
         System.out.println("Task 2");
-        for (int i = 10; i >= 0; i--) {
+        for (int i = 10; i >= 1; i--) {
             System.out.print(i + " ");
         }
-        System.out.println();
-        System.out.println("----------------------------");
+        System.out.println("\n----------------------------");
 
         System.out.println("Task 3");
 
-        for (int i = 0; i < 17; i = i + 2) {
+        for (int i = 0; i <= 17; i = i + 2) {
             System.out.print(i + " ");
         }
-        System.out.println();
-        System.out.println("----------------------------");
+        System.out.println("\n----------------------------");
 
         System.out.println("Task 4");
 
         for (int i = 10; i >= - 10; i--) {
             System.out.print(i + " ");
         }
-        System.out.println();
-        System.out.println("----------------------------");
+        System.out.println("\n----------------------------");
 
         System.out.println("Task 5");
 
@@ -46,16 +42,14 @@ public class Main {
         for (int i = 7; i <= 98; i = i + 7) {
             System.out.print(i + " ");
         }
-        System.out.println();
-        System.out.println("----------------------------");
+        System.out.println("\n----------------------------");
 
         System.out.println("Task 7");
 
         for (int i = 1; i <= 512; i<<=1) {
             System.out.print(i + " ");
         }
-        System.out.println();
-        System.out.println("----------------------------");
+        System.out.println("\n----------------------------");
 
         System.out.println("Task 8");
 
